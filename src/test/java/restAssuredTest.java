bce84dcc14a37815f18441973c750cbbe7bebf80
Sveta Andreeva <89111378147v@yandex.com>
2022-10-30 03:44:07 +0300

    get location redirect test added

---
+++ src/test/java/restAssuredTest.java
@@ -0,0 +1,26 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class restAssuredTest {
+    @Test
+    public void testRestAssured() {
+
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+
+        String name = response.getHeader("location");
+        System.out.println(name);
+
+    }
+
+}
